072046bb7bc9e5826343af9f3c70693a9b00c084
nasteho <nasteho.kahie@edu.turkuamk.fi>
2026-01-31 17:54:34 +0200

    first-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from './components/Home';
 
-import './App.css';
 const App = () => {
   return (
     <>
       <h1>My App</h1>
+      <Home />
     </>
   );
 };
-
 export default App;
